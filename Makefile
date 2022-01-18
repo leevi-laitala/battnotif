eafa6a29a4fb44144aab91df650d2fcbb30e0f8f
Leevi Laitala <leevi.laitala@protonmail.com>
2022-01-18 19:32:18 +0200

    Now in working state

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CC = gcc
 CFLAGS = -O3 -g3  -W -Wall -Wextra -Wuninitialized -Wstrict-aliasing -std=c11
 LIBS = -D_REENTRANT -I/usr/include/dbus-1.0 -I/usr/lib/dbus-1.0/include -lm -lpulse -pthread -ldbus-1
 
-cvolctrl: cvolctrl.c
+battnotif: battnotif.c
 	$(CC) $(CFLAGS) -o $(APP) $(LIBS) battnotif.c
 
 clean:
--- battnotif.c
+++ battnotif.c
@@ -2,27 +2,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdbool.h>
 #include <dbus-1.0/dbus/dbus.h>
 
 /* Battery notifier settigns */
-typedef enum { 
-    CHARGING,
-    DISCHARGING,
-    FULLCHARGE
-} ChargingStatus;
-
-typedef enum {
-    CFULL,
-    CNOTICE,
-    CLOW,
-    CCRIT
-} ChargeLevel;
-
-typedef struct {
-    int charge;
-    ChargingStatus status;
-} BatteryStatus;
-
 typedef struct {
     unsigned charge;
     const char* alert;
@@ -30,26 +13,39 @@ typedef struct {
 } Rule;
 
 static char* fileCapacity = "/sys/class/power_supply/BAT0/capacity";
-static char* fileAc       = "/sys/class/power_supply/AC/online"
-static int interval = 10; // as seconds
+static char* fileAc       = "/sys/class/power_supply/AC/online";
+static int interval = 3; // as seconds
 static Rule chargeLevels[] = {
-    {90, "full",            "battery_full"}, // Charge, notification, icon
-    {30, "notice",          "battery_30"},
-    {15, "low",             "battery_20"},
-    {5,  "critically low!", "battery_alert"}
+    {5,   "critically low!", "battery_alert"}, // Charge, notification, icon
+    {15,  "low",             "battery_20"},
+    {30,  "notice",          "battery_30"},
+
+    {90,  "full",            "battery_full"}, // Full at
+    {200, "charging",        "battery_charging_full"} // Charging
 };
+
+typedef enum { 
+    CHARGING,
+    DISCHARGING
+} ChargingStatus;
+
+typedef struct {
+    unsigned charge;
+    ChargingStatus status;
+    bool full;
+} BatteryStatus;
 
 /* Notification settings */
 enum { LOW, NORMAL, CRITICAL };
 static unsigned char level = LOW;
 static char* application = "battonotif";
 static char* urgency = "urgency";
-static char* icons[] = { "volume_mute", "volume_down", "volume_up", "volume_off" }; // Last icon is used when muted
 static int timeout = 1000 * 3; // as milliseconds
 static unsigned id = 2593;
 
 /* Globals */
 BatteryStatus battery;
+char ntitle[25], nbody[40];
 
 
 
@@ -107,27 +103,60 @@ void getBatteryStatus() {
     charging = fgetc(file);
     fclose(file);
     
-    if ((char)charging == '1')
-        if (battery.charge > chargeLevels[CFULL].charge)
-            battery.status = FULLCHARGE;
-        else
-            battery.status = CHARGING;
-    else
+    battery.full = (battery.charge >= chargeLevels[sizeof(chargeLevels) / sizeof(Rule) - 2].charge);
+
+    if ((char)charging == '1') {
+        battery.status = CHARGING;
+    } else {
         battery.status = DISCHARGING;
+    }
 }
 
-int main(int argc, const char* argv[]) {
-    unsigned lastChargeLevel = 0;
+void checkRules() {
+    getBatteryStatus();
+    BatteryStatus lastStatusAlert = { 100, battery.status, false };
     
     for (;;) {
-        printf("Tick\n");
-        //printf("Percent: %d\n", getBatteryPercentage());
-        getBatteryStatus();
-
-        if (battery.charge > )
-
         sleep(interval);
-    }
+        getBatteryStatus();
+        
+        if (battery.status == DISCHARGING) {
+            if (lastStatusAlert.status != DISCHARGING) { // Check if status wasnt discharging before
+                lastStatusAlert.status = DISCHARGING;
+                lastStatusAlert.full = false;
 
+                sprintf(nbody, "Battery charge at %d%%", battery.charge);
+                notify("Battery discharging", nbody, chargeLevels[(int)((battery.charge / 100) / (sizeof(chargeLevels) / sizeof(Rule)))].icon);
+            } else { // If status was discharging before
+                for (unsigned i = 0; i < (unsigned)(sizeof(chargeLevels) / sizeof(Rule)) - 2; i++) { // Loop through all rules, except last one as it's reserved for full battery check
+                    if (battery.charge <= chargeLevels[i].charge) { // Check if charge is less that set rule
+                        if (lastStatusAlert.charge != chargeLevels[i].charge) { // Check if alert hasn't already been notified
+                            lastStatusAlert.charge = chargeLevels[i].charge;
+
+                            sprintf(ntitle, "Battery %s", chargeLevels[i].alert);
+                            sprintf(nbody, "Battery charge at %d%%", battery.charge);
+                            notify(ntitle, nbody, chargeLevels[i].icon);
+                        }
+                        break;
+                    }
+                }
+            }
+        } else { // If battery is charging
+            if (!lastStatusAlert.full & battery.full) {
+                lastStatusAlert.full = true;
+                notify("Battery full", "Battery charge full", chargeLevels[sizeof(chargeLevels) / sizeof(Rule) - 2].icon);
+            } else if (lastStatusAlert.status != CHARGING) {
+                lastStatusAlert.status = CHARGING;
+                sprintf(nbody, "Battery charging at %d%%", battery.charge);
+                notify("Battery charging", nbody, chargeLevels[sizeof(chargeLevels) / sizeof(Rule) - 1].icon);
+            }
+
+        }
+    }
+}
+
+
+int main(void) {
+    checkRules();
     return 0;
 }
